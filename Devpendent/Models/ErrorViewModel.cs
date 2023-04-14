9814964ac57d64d9275b12403accd70416e55580
Samuel Ward <samuelleeward9@gmail.com>
2023-04-14 15:02:48 +0200

    Add project files.

---
+++ Devpendent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devpendent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
