45b178617660eac808141c429e80984a91a600e1
Samuel Ward <samuelleeward9@gmail.com>
2023-04-17 12:02:35 +0200

    Scaffolded Identity

---
+++ Devpendent/Areas/Identity/Data/DevpendentUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Devpendent.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the DevpendentUser class
+public class DevpendentUser : IdentityUser
+{
+}
+
--- Devpendent/Program.cs
+++ Devpendent/Program.cs
@@ -1,17 +1,38 @@
 using Devpendent.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Devpendent.Data;
+using Devpendent.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DevpendentContextConnection") ?? throw new InvalidOperationException("Connection string 'DevpendentContextConnection' not found.");
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.IsEssential = true;
+});
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
 });
 
+builder.Services.AddDbContext<DevpendentContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration[connectionString]);
+});
+
+builder.Services.AddDefaultIdentity<DevpendentUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DevpendentContext>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,12 +46,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
