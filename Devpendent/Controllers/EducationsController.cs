b80c55c8e45b16eafc2809f5ffd18cbadf5025e2
Samuel Ward <samuelleeward9@gmail.com>
2023-04-26 18:59:01 +0200

    Added category view and additional CRUD

---
+++ Devpendent/Controllers/EducationsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Devpendent.Data;
+using Devpendent.Models;
+
+namespace Devpendent.Controllers
+{
+    public class EducationsController : Controller
+    {
+        private readonly DevpendentContext _context;
+
+        public EducationsController(DevpendentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Educations
+        public async Task<IActionResult> Index()
+        {
+            var devpendentContext = _context.Educations.Include(e => e.User);
+            return View(await devpendentContext.ToListAsync());
+        }
+
+        // GET: Educations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Educations == null)
+            {
+                return NotFound();
+            }
+
+            var education = await _context.Educations
+                .Include(e => e.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (education == null)
+            {
+                return NotFound();
+            }
+
+            return View(education);
+        }
+
+        // GET: Educations/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Educations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,UniversityName,GraduationYear,UserId")] Education education)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(education);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", education.UserId);
+            return View(education);
+        }
+
+        // GET: Educations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Educations == null)
+            {
+                return NotFound();
+            }
+
+            var education = await _context.Educations.FindAsync(id);
+            if (education == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", education.UserId);
+            return View(education);
+        }
+
+        // POST: Educations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,UniversityName,GraduationYear,UserId")] Education education)
+        {
+            if (id != education.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(education);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EducationExists(education.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", education.UserId);
+            return View(education);
+        }
+
+        // GET: Educations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Educations == null)
+            {
+                return NotFound();
+            }
+
+            var education = await _context.Educations
+                .Include(e => e.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (education == null)
+            {
+                return NotFound();
+            }
+
+            return View(education);
+        }
+
+        // POST: Educations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Educations == null)
+            {
+                return Problem("Entity set 'DevpendentContext.Educations'  is null.");
+            }
+            var education = await _context.Educations.FindAsync(id);
+            if (education != null)
+            {
+                _context.Educations.Remove(education);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EducationExists(int id)
+        {
+            return _context.Educations.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Devpendent/Infrastructure/Components/CategoriesViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Devpendent.Data;
+using Devpendent.Migrations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Devpendent.Infrastructure.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly DevpendentContext _context;
+
+        public CategoriesViewComponent(DevpendentContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categories.ToListAsync());
+    }
+}
