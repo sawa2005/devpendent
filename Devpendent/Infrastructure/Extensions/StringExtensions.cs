bacf80d9aae3df4220712639797a2fc93a5359e1
Samuel Ward <samuelleeward9@gmail.com>
2023-05-22 16:55:51 +0200

    Truncate extension method and project browse layout

---
+++ Devpendent/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Devpendent.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (value == null) return value;
+
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
+}
