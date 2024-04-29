46bc955aa00d9d2650361b0831067c73c54f7c5f
Samuel Ward <samuelleeward9@gmail.com>
2024-04-29 16:19:19 +0200

    Home page background animation added

--- Devpendent/wwwroot/js/site.js
+++ Devpendent/wwwroot/js/site.js
@@ -3,6 +3,123 @@
 
 // Write your JavaScript code.
 
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+let mouseMoved = false;
+let mouseStopped = true;
+
+const debounce = (callback, wait) => {
+    let timeoutId = null;
+    return (...args) => {
+        window.clearTimeout(timeoutId);
+        timeoutId = window.setTimeout(() => {
+            callback(args);
+        }, wait);
+    };
+}
+
+const pointer = {
+    x: 0.5 * window.innerWidth,
+    y: 0.5 * window.innerHeight
+};
+
+const params = {
+    pointsNumber: 40,
+    widthFactor: 10,
+    mouseThreshold: 0.5,
+    spring: 0.25,
+    friction: 0.5
+};
+
+const trail = new Array(params.pointsNumber);
+
+for (let i = 0; i < params.pointsNumber; i++) {
+    trail[i] = {
+        x: pointer.x,
+        y: pointer.y,
+        dx: 0,
+        dy: 0
+    };
+}
+
+window.addEventListener("click", (e) => {
+    updateMousePosition(e.pageX, e.pageY);
+});
+
+window.addEventListener("mousemove", (e) => {
+    mouseMoved = true;
+    mouseStopped = false;
+    updateMousePosition(e.pageX, e.pageY);
+});
+
+document.addEventListener("mousemove", debounce(() => mouseStopped = true, 1000));
+
+window.addEventListener("touchmove", (e) => {
+    mouseMoved = true;
+    updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+});
+
+function updateMousePosition(eX, eY) {
+    pointer.x = eX;
+    pointer.y = eY;
+}
+
+setupCanvas();
+update(0);
+
+window.addEventListener("resize", setupCanvas);
+
+function update(t) {
+    if (mouseStopped) {
+        pointer.x = (0.5 + 0.3 * Math.cos(0.002 * t) * Math.sin(0.005 * t)) * window.innerWidth;
+        pointer.y = (0.5 + 0.2 * Math.cos(0.005 * t) + 0.1 * Math.sin(0.01 * t)) * window.innerHeight;
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    trail.forEach((p, pIdx) => {
+        const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
+        const spring = pIdx === 0 ? 0.4 * params.spring : params.spring;
+
+        p.dx += (prev.x - p.x) * spring;
+        p.dy += (prev.y - p.y) * spring;
+        p.dx *= params.friction;
+        p.dy *= params.friction;
+        p.x += p.dx;
+        p.y += p.dy;
+    });
+
+    var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+
+    gradient.addColorStop(0, "#3f6739");
+    gradient.addColorStop(1, "#20a761");
+
+    ctx.strokeStyle = gradient;
+    ctx.linecap = "round";
+    ctx.beginPath();
+    ctx.moveTo(trail[0].x, trail[0].y);
+
+    for (let i = 1; i < trail.length - 1; i++) {
+        const xc = 0.5 * (trail[i].x + trail[i + 1].x);
+        const yc = 0.5 * (trail[i].y + trail[i + 1].y);
+
+        ctx.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
+        ctx.lineWidth = params.widthFactor * (params.pointsNumber - i);
+        ctx.stroke();
+    }
+
+    ctx.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
+    ctx.stroke();
+
+    window.requestAnimationFrame(update);
+}
+
+function setupCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
 $(function () {
     if ($("a.confirmDeletion").length) {
         $("a.confirmDeletion").click(() => {
